ec8d37967498e0a437189b59b4ba1d40975fdbe8
Max <max@motovilov.com>
2013-10-30 22:00:30 -0500

    Use spaces instead of tabs

--- umd.js
+++ umd.js
@@ -1,38 +1,37 @@
 if( typeof define === "function" )
-	define( [ 'require', 'module' ],
-    		function( require, module ) {
-				return {
-					usingAMD: require.undef && require.defined && require.specified && "requirejs" || true,
+    define( [ 'require', 'module' ],
+            function( require, module ) {
+                return {
+                    usingAMD: require.undef && require.defined && require.specified && "requirejs" || true,
 
-					isRequire: function( r ) {
-						return typeof r === "function" && typeof r.toUrl === "function";
-					},
-				
-					isModule: function( m ) {
-						return typeof m === "object" && typeof m.id === "string" && require( m.id );
-					}
-				}
-			}
-	);
+                    isRequire: function( r ) {
+                        return typeof r === "function" && typeof r.toUrl === "function";
+                    },
+                
+                    isModule: function( m ) {
+                        return typeof m === "object" && typeof m.id === "string" && require( m.id );
+                    }
+                }
+            }
+    );
 else if( require && module && exports ) {
 
-	exports.usingCommonJS = require.cache && "node" || true;
-	
-	exports.isRequire = function( r ) {
-		return typeof r === "function" && (
-		   require.cache 
-				? require.cache === r.cache 	// Node.js
-				: require.paths === r.paths		// CommonJS Modules/1.1.1
-		);
-	}
+    exports.usingCommonJS = require.cache && "node" || true;
+    
+    exports.isRequire = function( r ) {
+        return typeof r === "function" && (
+           require.cache 
+                ? require.cache === r.cache 	// Node.js
+                : require.paths === r.paths		// CommonJS Modules/1.1.1
+        );
+    }
 
-	exports.isModule = function( m ) {
-		return typeof m === "object" && (
-			require.cache 
-				? m.id in require.cache							// Node.js
-				: typeof m.id === "string" && require( m.id )	// CommonJS Modules/1.1.1
-		);
-	}
-}
-else
-	throw Error( "Loader type cannot be determined" );
+    exports.isModule = function( m ) {
+        return typeof m === "object" && (
+            require.cache 
+                ? m.id in require.cache							// Node.js
+                : typeof m.id === "string" && require( m.id )	// CommonJS Modules/1.1.1
+        );
+    }
+} else
+    throw Error( "Loader type cannot be determined" );
